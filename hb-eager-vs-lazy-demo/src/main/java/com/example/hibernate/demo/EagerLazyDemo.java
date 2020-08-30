2402d9477304cb0fa883961de166900e55d47116
Zohra Khan <erzohrakhan@gmail.com>
2020-08-30 17:13:34 +0530

    Demo of Eager loading in One to Many mappings

---
+++ hb-eager-vs-lazy-demo/src/main/java/com/example/hibernate/demo/EagerLazyDemo.java
@@ -0,0 +1,59 @@
+package com.example.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.example.hibernate.demo.entity.Course;
+import com.example.hibernate.demo.entity.Instructor;
+import com.example.hibernate.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+		// Create Session factory
+		/*
+		 * To achieve this purpose, we need to have a ServiceRegistry that holds the
+		 * Services needed by Hibernate. From this registry, we can build a Metadata
+		 * object that represents the application's domain model and its mapping to the
+		 * database.
+		 */
+		// Configuration con = new
+		// Configuration().configure().addAnnotatedClass(Alien.class);
+
+		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+
+		SessionFactory sf = new MetadataSources(registry).addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildMetadata()
+				.buildSessionFactory();
+		Session session = sf.getCurrentSession();
+
+		try {
+
+			// create a student object
+			System.out.println("Creating a new Instructor object..");
+
+			// start a transcation
+			Transaction tx = session.beginTransaction();
+
+			// Get the instructor from db
+			int id = 1;
+			Instructor instructor = session.get(Instructor.class, id);
+			System.out.println(instructor);
+
+			System.out.println(instructor.getCourses());
+
+			System.out.println("Saving the courses");
+
+			tx.commit();
+			System.out.println("Done!");
+		} finally {
+			session.close();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+}
---
+++ hb-eager-vs-lazy-demo/src/main/java/com/example/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,115 @@
+package com.example.hibernate.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String first_name;
+
+	@Column(name = "last_name")
+	private String last_name;
+
+	@Column(name = "email")
+	private String email;
+
+	@OneToMany(fetch = FetchType.EAGER, mappedBy = "instructor", cascade = { CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH, CascadeType.DETACH })
+	private List<Course> courses;
+
+	// Set up mapping between instructor and instructor_detail
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "instructor_detail_id")
+	private InstructorDetail instructorDetail;
+
+	public Instructor() {
+	}
+
+	public Instructor(String first_name, String last_name, String email) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+
+	public void add(Course course) {
+		if (courses == null)
+			courses = new ArrayList<Course>();
+
+		courses.add(course);
+		course.setInstructor(this);
+
+	}
+}
